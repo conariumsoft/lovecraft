dd6c62385c207362b21b6e7e758acf289b7176e2
scientiist <j0sh.oleary11@gmail.com>
2020-05-02 02:50:59 -0500

    fat

--- game/ReplicatedStorage/ItemMetadata/BaseFirearm.lua
+++ game/ReplicatedStorage/ItemMetadata/BaseFirearm.lua
@@ -2,21 +2,85 @@ local BaseInteractive = require(script.Parent.BaseInteractive)
 
 local BaseFirearm = BaseInteractive:subclass("BaseFirearm")
 
+BaseFirearm.TriggerStiffness = 0.95
+BaseFirearm.RateOfFire = 850
+
+BaseFirearm.MuzzleFlipMax = 150
+BaseFirearm.MuzzleFlipMin = 50
+BaseFirearm.YShakeMin = -15
+BaseFirearm.YShakeMax = 15
+BaseFirearm.ZShakeMin = -10
+BaseFirearm.ZShakeMax = 10
+BaseFirearm.RecoilRecoverySpeed = 5
+BaseFirearm.BarrelComponent = nil
+BaseFirearm.BarrelComponentName = nil
+BaseFirearm.Timer = 0
+BaseFirearm.MagazineType = nil
+BaseFirearm.OpenBolt = false
+
+function BaseFirearm:GetCycleTime()
+    return self.RateOfFire/60
+end
+
 function BaseFirearm:OnGrab(hand, model, grip_point)
+
+    self.BarrelComponent = model[self.BarrelComponentName]
+
+    local muzzle_flip = Instance.new("BodyThrust") do
+        muzzle_flip.Name = "MuzzleFlip"
+        muzzle_flip.Location = Vector3.new(0, 0, 0)
+        muzzle_flip.Force = Vector3.new(0, 0, 0)
+        muzzle_flip.Parent = self.BarrelComponent
+    end
+
+
+    local recoil_impulse = Instance.new("BodyThrust") do
+        recoil_impulse.Name = "RecoilImpulse"
+        recoil_impulse.Location = Vector3.new(0, 0, 0)
+        recoil_impulse.Parent = self.BarrelComponent
+        recoil_impulse.Force = Vector3.new(0, 0, 0)
+    end
 
 end
 
 function BaseFirearm:OnRelease(hand, model, grip_point)
-
+    self.BarrelComponent.MuzzleFlip:Destroy()
+    self.BarrelComponent.RecoilImpulse:Destroy()
 end
 
-
 function BaseFirearm:OnSimulationStep(hand, model, dt, grip_point)
+    -- trigger being pulled
+    if hand.IndexFingerPressure > self.TriggerStiffness then
+        self.Timer = self.Timer + dt
+        if self.Timer >= (1/self:GetCycleTime()) then
+            self.Timer = self.Timer - (1/self:GetCycleTime())
+            
+            model.fire:Stop()
+            model.fire.TimePosition = 0.05
+            model.fire:Play()
 
+            -- TODO: Backwards recoil!
+            local recoil_impulse = model.rifling.RecoilImpulse
+
+            --muzzle_flip.Force = Vector3.new(0, 0, 50)
+            recoil_impulse.Force = Vector3.new(
+                -math.max(  self.MuzzleFlipMin, self.MuzzleFlipMax), 
+                math.random(self.YShakeMin,     self.YShakeMax    ), 
+                math.random(self.ZShakeMin,     self.ZShakeMax    )
+            )
+
+            delay(1/self.RecoilRecoverySpeed, function()
+                recoil_impulse.Force = Vector3.new(0, 0, 0)
+            end)
+
+        end
+    end
 end
 
 function BaseFirearm:OnTriggerState(hand, model, finger_pressure, grip_point)
-
+    if finger_pressure > 0.95 then
+        
+    end
 end
 
 return BaseFirearm
--- game/ReplicatedStorage/ItemMetadata/Skorpion.lua
+++ game/ReplicatedStorage/ItemMetadata/Skorpion.lua
@@ -14,80 +14,16 @@ local Skorpion = BaseFirearm:subclass("Skorpion")
 
 ]]
 
-local timer = (1/10)
-
-function Skorpion:OnGrab(hand, model, grip_point)
-    self.super:OnGrab(hand, model, grip_point)
-    local muzzle_flip = Instance.new("BodyThrust") do
-        muzzle_flip.Name = "MuzzleFlip"
-        muzzle_flip.Location = Vector3.new(0, 0, 0)
-        muzzle_flip.Force = Vector3.new(0, 0, 0)
-        muzzle_flip.Parent = model.rifling
-    end
-    local recoil_impulse = Instance.new("BodyThrust") do
-        recoil_impulse.Name = "RecoilImpulse"
-        recoil_impulse.Location = Vector3.new(0, 0, 0)
-        recoil_impulse.Parent = model.rifling
-        recoil_impulse.Force = Vector3.new(0, 0, 0)
-    end
-end
-
-function Skorpion:OnRelease(hand, model, grip_point)
-    self.super:OnRelease(hand, model, grip_point)
-    model.rifling.MuzzleFlip:Destroy()
-    model.rifling.RecoilImpulse:Destroy()
-end
-
-local trigger_stiffness = 0.95
-local rate_of_fire = 850 -- rounds per minute
-
-local muzzle_flip_max = 90
-local muzzle_flip_min = 70
-
-local y_shake_min = -15
-local y_shake_max =  15
-
-local z_shake_min = -10
-local z_shake_max = 10
-
-local recoil_recovery_speed = 10
-
-local cycle_time = (rate_of_fire/60)
-
-function Skorpion:OnSimulationStep(hand, model, dt, grip_point)
-    -- trigger being pulled
-    if hand.IndexFingerPressure > trigger_stiffness then
-        timer = timer + dt
-        if timer >= (1/cycle_time) then
-            timer = timer - (1/cycle_time)
-            
-            model.fire:Stop()
-            model.fire.TimePosition = 0.05
-            model.fire:Play()
-
-            -- TODO: Backwards recoil!
-            local recoil_impulse = model.rifling.RecoilImpulse
-
-            --muzzle_flip.Force = Vector3.new(0, 0, 50)
-            recoil_impulse.Force = Vector3.new(
-                -math.max(   muzzle_flip_min, muzzle_flip_max), 
-                 math.random(y_shake_min,     y_shake_max    ), 
-                 math.random(z_shake_min,     z_shake_max    )
-            )
-
-            delay(1/recoil_recovery_speed, function()
-                recoil_impulse.Force = Vector3.new(0, 0, 0)
-            end)
-
-        end
-    end
-    
-end
-
-function Skorpion:OnTriggerState(hand, model, finger_pressure, grip_point)
-    if finger_pressure > 0.95 then
-        
-    end
-end
+Skorpion.TriggerStiffness = 0.95
+Skorpion.RateOfFire = 850
+Skorpion.MuzzleFlipMax = 90
+Skorpion.MuzzleFlipMin = 70
+Skorpion.YShakeMin = -15
+Skorpion.YShakeMax = 15
+Skorpion.ZShakeMin = -10
+Skorpion.ZShakeMax = 10
+Skorpion.RecoilRecoverySpeed = 10
+Skorpion.BarrelComponentName = "rifling"
+Skorpion.Timer = 0
 
 return Skorpion
