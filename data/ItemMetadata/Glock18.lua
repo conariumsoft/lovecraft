4c12509ac75e82a60d27d4f4d04c16d07ffec094
scientiist <j0sh.oleary11@gmail.com>
2020-05-26 17:17:43 -0500

    Added Glock18

---
+++ data/ItemMetadata/Glock18.lua
@@ -0,0 +1,15 @@
+local Glock17 = require(script.Parent.Glock17)
+
+local Glock18 = Glock17:subclass("Glock18")
+
+--[[
+    GLOCK 18 Automatic pistol
+    https://en.wikipedia.org/wiki/Glock
+
+    Cartridge = 9mm Para
+    Rate of Fire = N/A (Gonna go with 1000)
+]]
+
+Glock18.Automatic = true
+
+return Glock18
